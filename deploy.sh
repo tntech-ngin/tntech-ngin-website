38ef93aace65b867a2729e351b616bff1a7e693b
susmit85 <susmit.shannigrahi@gmail.com>
2020-05-16 21:00:52 -0500

    rebuilding site Sat 16 May 2020 09:00:52 PM CDT

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo -e "Deploying updates to GitHub..."
+
+# Build the project.
+/usr/local/bin/hugo  # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd nextgen-ftp.github.io
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git pull origin master --rebase
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
+
+git push origin master
